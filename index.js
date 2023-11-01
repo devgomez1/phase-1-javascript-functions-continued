c5d7c43a8405a328dc57f94aec8cb82dee32f362
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-01 09:45:06 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = 'go to the office'){
+    return (`This Monday, I will ${activity}.`);
+}
+
+mondayWork();
+mondayWork('work from home');
+
+function wrapAdjective(string = "*"){
+ return function innerfunction(message = "special"){
+    return(`You are ${string}${message}${string}!`)
+ }
+}
+
